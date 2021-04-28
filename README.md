070d13ef7b72de4b486ce6d537b9341cc45a1244
Michael Grace <michael.dp.grace@outlook.com>
2021-04-28 21:40:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Roses VOD API
+
+### Running
+* `python3 -m venv .venv`
+* `source .venv/bin/activate`
+* `pip install -r requirements.txt`
+* `python3 app.py`
+
+### API
+
+* `GET /all_vods`
+
+Response: 
+```
+{
+"vods": [
+            {
+                "id": 123,
+                "title": "VOD Title",
+                "description": "VOD Description",
+                "url": "vod-url"
+            }
+    ]
+}
+```
+
+* `POST /vod`
+
+To Describe
+
+* `PATCH /vod/id`
+
+To Describe
+
+### TODO (on Thursday)
+* Interface
---
+++ app.py
@@ -0,0 +1,86 @@
+from aiohttp import web
+import json
+import typing as T
+import random
+import config
+
+vods: T.List[T.Dict[str, T.Union[str, int]]] = []
+
+def writeback():
+    with open("data.json", "w") as f:
+        f.write(json.dumps(vods))
+
+async def get_all_vods(_: web.Request) -> web.Response:
+    return web.json_response({
+        "vods": vods
+    })
+
+async def add_vod(req: web.Request) -> web.Response:
+    data: T.Dict[str, str] = await req.json()
+    for part in ["title", "description", "url"]:
+        if part not in data:
+            raise web.HTTPBadRequest
+        
+    global vods
+
+    new_vod = {
+        "id": random.randint(0, 1000000), # Lets hope theres no conflicts
+        "title": data["title"],
+        "description": data["description"],
+        "url": data["url"]
+    }
+
+    vods.append(new_vod)
+
+    writeback()
+
+    return web.json_response(new_vod)
+
+async def update_vod(req: web.Request) -> web.Response:
+    data: T.Dict[str, str] = await req.json()
+    for part in ["title", "description", "url"]:
+        if part not in data:
+            raise web.HTTPBadRequest
+
+    try:
+        id = int(req.match_info["id"])
+    except (ValueError, KeyError):
+        raise web.HTTPBadRequest
+
+    global vods
+
+    # Check ID Exists
+    for vod in vods:
+        if vod["id"] == id:
+            break
+    else:
+        raise web.HTTPNotFound
+    
+    updated_vod = {
+            "id": id,
+            "title": data["title"],
+            "description": data["description"],
+            "url": data["url"]
+        }
+
+    vods = [x if x["id"] != id
+        else updated_vod for x in vods]
+
+    writeback()
+
+    return web.json_response(updated_vod)
+  
+
+
+with open("data.json") as f:
+    vods = json.load(f)
+
+app = web.Application()
+app.add_routes([
+    web.get("/all_vods", get_all_vods),
+    web.post("/vod", add_vod),
+    web.patch("/vod/{id}", update_vod)
+])
+
+print("Starting Server")
+web.run_app(app, port=config.PORT)
