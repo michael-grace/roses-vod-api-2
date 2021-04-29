26a3a112269529f152996f1c075dd904b23c72e3
Michael Grace <michael.dp.grace@outlook.com>
2021-04-29 01:27:09 +0100

    Documentation and Formatting

--- README.md
+++ README.md
@@ -8,33 +8,91 @@
 
 ### API
 
+---
+
 * `GET /all_vods`
 
 Response: 
-```
+```json
 {
-"vods": [
-            {
-                "id": 123,
-                "title": "VOD Title",
-                "description": "VOD Description",
-                "url": "vod-url"
-            }
+    "vods": [
+        {
+            "id": 123,
+            "title": "VOD Title",
+            "description": "VOD Description",
+            "url": "vod-url"
+        }
     ]
 }
 ```
 
+---
+
 * `POST /vod`
 
-To Describe
+Request Body:
+```json
+{
+    "title": "New VOD Title",
+    "description": "New VOD Description",
+    "url": "New VOD URL"
+}
+```
 
-* `PUT /vod/id`
+OK Response:
+```json
+{
+    "id": 123,
+    "title": "New VOD Title",
+    "description": "New VOD Description",
+    "url": "New VOD URL"
+}
+```
 
-To Describe
+---
 
-* `DELETE /vod/id`
+* `PUT /vod/{id}`
 
-To Describe
+Parameters:
+* `id`: The ID number of the VOD to be edited
 
-### TODO (on Thursday)
-* Interface
+Request Body:
+```json
+{
+    "title": "New VOD Title",
+    "description": "New VOD Description",
+    "url": "New VOD URL"
+}
+```
+
+*Note:* All fields must be provided, even if the data doesn't change. A `400 Bad Request` response will be returned otherwise
+
+OK Response:
+```json
+{
+    "id": 123,
+    "title": "New VOD Title",
+    "description": "New VOD Description",
+    "url": "New VOD URL"
+}
+```
+
+---
+
+* `DELETE /vod/{id}`
+
+Parameters:
+* `id`: The ID number of the VOD to be edited
+
+OK Response:
+```json
+{
+    "Deleted": 123
+}
+```
+
+---
+
+### Interface
+
+Available at `/manage`
--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 from aiohttp import web
 import aiohttp_cors
+
 import json
 import typing as T
-import random
+import time
+
 import config
 
+
 vods: T.List[T.Dict[str, T.Union[str, int]]] = []
+
 
 def writeback():
     with open("data.json", "w") as f:
         f.write(json.dumps(vods))
+
 
 async def get_all_vods(_: web.Request) -> web.Response:
     return web.json_response({
         "vods": vods
     })
 
+
 async def add_vod(req: web.Request) -> web.Response:
     data: T.Dict[str, str] = await req.json()
     for part in ["title", "description", "url"]:
         if part not in data:
             raise web.HTTPBadRequest
-        
+
     global vods
 
     new_vod = {
-        "id": random.randint(0, 1000000), # Lets hope theres no conflicts
+        "id": int(time.time()),
         "title": data["title"],
         "description": data["description"],
         "url": data["url"]
@@ -36,6 +42,7 @@ async def add_vod(req: web.Request) -> web.Response:
     writeback()
 
     return web.json_response(new_vod)
+
 
 async def update_vod(req: web.Request) -> web.Response:
     data: T.Dict[str, str] = await req.json()
@@ -56,21 +63,21 @@ async def update_vod(req: web.Request) -> web.Response:
             break
     else:
         raise web.HTTPNotFound
-    
+
     updated_vod = {
-            "id": id,
-            "title": data["title"],
-            "description": data["description"],
-            "url": data["url"]
-        }
+        "id": id,
+        "title": data["title"],
+        "description": data["description"],
+        "url": data["url"]
+    }
 
     vods = [x if x["id"] != id
-        else updated_vod for x in vods]
+            else updated_vod for x in vods]
 
     writeback()
 
     return web.json_response(updated_vod)
-        
+
 
 async def delete_vod(req: web.Request) -> web.Response:
     try:
@@ -95,7 +102,8 @@ async def delete_vod(req: web.Request) -> web.Response:
     return web.json_response({
         "Deleted": id
     })
-  
+
+
 async def manage_redirect(_: web.Request) -> web.Response:
     raise web.HTTPFound("/manage/index.html")
 
@@ -105,7 +113,8 @@ with open("data.json") as f:
 app = web.Application()
 cors = aiohttp_cors.setup(app)
 
-cors.add(app.router.add_get("/all_vods", get_all_vods), { #type: ignore
+# CORS
+cors.add(app.router.add_get("/all_vods", get_all_vods), {  # type: ignore
     "https://roseslive.co.uk": aiohttp_cors.ResourceOptions(
         allow_credentials=True,
         allow_headers="*",
